0f352b378ea3b34ee577dffb852bc8335d36d426
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-11-14 12:21:42 +0330

    Update resnet_kd.py

--- resnet_kd.py
+++ resnet_kd.py
@@ -1,27 +1,3 @@
-# borrow from https://github.com/akamaster/pytorch_resnet_cifar10/blob/master/resnet.py
-'''
-Properly implemented ResNet-s for CIFAR10 as described in paper [1].
-The implementation and structure of this file is hugely influenced by [2]
-which is implemented for ImageNet and doesn't have option A for identity.
-Moreover, most of the implementations on the web is copy-paste from
-torchvision's resnet and has wrong number of params.
-Proper ResNet-s for CIFAR10 (for fair comparision and etc.) has following
-number of layers and parameters:
-name      | layers | params
-ResNet20  |    20  | 0.27M
-ResNet32  |    32  | 0.46M
-ResNet44  |    44  | 0.66M
-ResNet56  |    56  | 0.85M
-ResNet110 |   110  |  1.7M
-ResNet1202|  1202  | 19.4m
-which this implementation indeed has.
-Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-[2] https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
-If you use this implementation in you work, please don't forget to mention the
-author, Yerlan Idelbayev.
-'''
 # model/resnet_standard.py
 import torch.nn as nn
 import torch.nn.functional as F
@@ -43,7 +19,7 @@ class LambdaLayer(nn.Module):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1, option='A'):
+    def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -52,15 +28,11 @@ class BasicBlock(nn.Module):
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
-            if option == 'A':
-                self.shortcut = LambdaLayer(
-                    lambda x: F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, (planes - in_planes) // 2, planes - in_planes - (planes - in_planes) // 2), "constant", 0)
-                )
-            elif option == 'B':
-                self.shortcut = nn.Sequential(
-                    nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
-                    nn.BatchNorm2d(planes)
-                )
+            self.shortcut = LambdaLayer(
+                lambda x: F.pad(x[:, :, ::2, ::2],
+                                (0, 0, 0, 0, (planes - in_planes) // 2, planes - in_planes - (planes - in_planes) // 2),
+                                "constant", 0)
+            )
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
@@ -119,13 +91,11 @@ def resnet110(num_classes=10):
 if __name__ == "__main__":
     in_cfg = [3, 16, 16, 16, 32, 32, 32, 64, 64, 64]
     out_cfg = [16, 16, 16, 32, 32, 32, 64, 64, 64, 64]
-    model = resnet20(finding_masks=True, in_cfg=in_cfg, out_cfg=out_cfg)
+    model = resnet20(num_classes=10)
     print(model)
-    input = torch.rand((2, 3, 32, 32))
-    print(model(input))
-    model.hook_masks()
+    x = torch.randn(2, 3, 32, 32)
+    y = model(x)
+    print(y.shape)
 
-    masks_weights = model.get_masks()
-    print(masks_weights['mask.0'].mask)
 
 
